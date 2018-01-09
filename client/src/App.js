9a8ab3d210a4aa2f2ee875c907adb8aa94c0c3af
Tom Moir <trmoir9@gmail.com>
2018-01-09 11:09:11 +0000

    first commit
    setup Express and React connection

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { users: [] }
+  }
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        <ul>
+          {this.state.users.map(user =>
+            <li key={user.id}>{user.name}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ routes/users.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send('respond with a resource');
+
+  res.json([{
+    id: 1,
+    name: 'Eva'
+  }, {
+    id: 2,
+    name: 'Dania'
+  }, {
+    id: 3,
+    name: 'Lewis'
+  }, {
+    id: 4,
+    name: 'Tom'
+  }])
+});
+
+module.exports = router;
