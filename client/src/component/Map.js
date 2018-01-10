c7ffb31dbf5c77e30e270564e70c7d53f3d126aa
Lewis <lewisyoul@gmail.com>
2018-01-10 16:01:33 +0000

    Add point layer to Map
    Added the first working layer to the map using react-mapbox-js

--- client/src/component/Map.js
+++ client/src/component/Map.js
@@ -1,75 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import mapboxgl from 'mapbox-gl'
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+// mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
-const geojson = {
-  type: 'FeatureCollection',
-  features: [{
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-77.032, 38.913]
-    },
-    properties: {
-      title: 'Mapbox',
-      description: 'Washington, D.C'
-    }
-  }]
-};
-
-
-class Map extends React.Component {
-
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      lng: -0.07328080000002046,
-      lat: 51.5173403,
-      zoom: 9
-    };
-  }
+class Map extends Component {
 
   componentDidMount() {
-    const { lng, lat, zoom } = this.state;
-    console.log(this)
-    const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9',
-      center: [lng, lat],
-      zoom
-    });
+    MapboxGl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA'
 
-    map.on('move', () => {
-      const { lng, lat } = map.getCenter();
-
-      this.setState({
-        lng: lng.toFixed(10),
-        lat: lat.toFixed(10),
-        zoom: map.getZoom().toFixed(2)
-      });
-    });
+    new MapboxGl.Map({
+      container: this.container,
+      style: 'mapbox://styles/mapbox/light-v9'
+    })
   }
 
-
-
-
-
   render() {
-    const { lng, lat, zoom } = this.state;
-
     return (
-      <div>
-        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
-          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
-        </div>
-        <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+      <div className='Map' ref={(x) => { this.container = x }}>
       </div>
-    );
+    )
   }
 }
 
-export default Map;
-
-// ReactDOM.render(<Application />, document.getElementById('app'));
+export default Map
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './component/App';
-import Map from './component/Map';
+// import Map from './component/Map';
 import registerServiceWorker from './registerServiceWorker';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
-ReactDOM.render(<Map />, document.getElementById('root'));
+const Map = ReactMapboxGl({
+  accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA'
+});
+
+
+
+
+ReactDOM.render(<Map
+  style="mapbox://styles/mapbox/streets-v9"
+  containerStyle={{
+    height: "100vh",
+    width: "100vw"
+  }}>
+    <Layer
+      type="symbol"
+      id="marker"
+      layout={{ "icon-image": "marker-15", "icon-size": 5 }}>
+      <Feature coordinates={[-0.481747846041145, 51.3233379650232]}
+               onHover={this._onHover}
+               onEndHover={this._onEndHover}
+               onClick={this._onClickMarker}
+      />
+    </Layer>
+</Map>
+
+      , document.getElementById('root'));
 registerServiceWorker();
