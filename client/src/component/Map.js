b52a266758f24920db585f04d57a2679981739c3
Lewis <lewisyoul@gmail.com>
2018-01-10 12:03:25 +0000

    add geoJSON data to Map

--- client/src/component/Map.js
+++ client/src/component/Map.js
@@ -36,6 +36,32 @@ class Map extends React.Component {
     });
   }
 
+  var geojson = {
+  type: 'FeatureCollection',
+  features: [{
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-77.032, 38.913]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'Washington, D.C.'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-122.414, 37.776]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'San Francisco, California'
+    }
+  }]
+};
+
   render() {
     const { lng, lat, zoom } = this.state;
 
