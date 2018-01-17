e6b52e1e0144fc2bf9f933858d4f61b77db6d6ef
Lewis <lewisyoul@gmail.com>
2018-01-17 18:13:23 +0000

    Merged alterations in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,8 @@ import App from './component/App'
 import Geocoder from './component/Geocoder'
 import ReactMapboxGl from 'react-mapbox-gl';
 
-import Pin from './Pin'
-import PinPopup from './PinPopup'
+import Pin from './component/Pin'
+import PinPopup from './component/PinPopup'
 
 const Map = ReactMapboxGl({
   accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA'
