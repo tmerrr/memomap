43de457ec4c891457ec79006203274eee232cda9
thatdania@gmail.com <thatdania@gmail.com>
2018-01-18 12:46:18 +0000

    Add background for login page
    login page done

--- client/src/component/login.js
+++ client/src/component/login.js
@@ -12,17 +12,18 @@ export default class LogIn extends Component {
   render() {
     return(
       <div id="body">
-      <div class='fbmask'></div>
-      <h1 class='slogans'> Places • Pictures • Memories </h1>
+      <img id="background" src="back.png"/>
+      <img id="logo" src="memomap.png"/>
+      <h1 className='slogans'> Places • Pictures • Memories </h1>
+      <div className='fbmask'></div>
       <div id="fb"><FacebookLogin
         appId="1382418925218617"
         autoLoad={false}
         fields="name,email,friends,picture"
         scope="public_profile,email,user_friends"
         callback={this.props.responseFacebook}
-        // reAuthenticate={true}
       /></div>
-      <a class="change" href="https://www.facebook.com/" target="_blank">Change User</a>
+      <a className="change" href="https://www.facebook.com/" target="_blank">Change User</a>
       </div>
     )
   }
